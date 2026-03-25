a6e3f4609fe226cd46db01f42107172d9120fd12
Vanshika Bobade <vanshikabobade0@gmail.com>
2026-03-25 11:02:14 +0530

    Hero section

---
+++ portfolio-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero/Hero'
+
+function App() {
+  return (
+    <div className="bg-[#0F172A] text-[#F5F5F5]">
+      {/* 
+        This is a boilerplate App container showing the Hero component.
+        You can expand it to add your other Portfolio sections below the Hero.
+      */}
+      <Hero />
+    </div>
+  )
+}
+
+export default App
